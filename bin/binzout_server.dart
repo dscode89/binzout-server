052dd70c5a45b620679e0f4d42ea37f9c5538094
dscode89 <dscode89@gmail.com>
2025-12-10 15:27:24 +0000

    initial commit

---
+++ bin/binzout_server.dart
@@ -0,0 +1,5 @@
+import 'package:binzout_server/binzout_server.dart' as binzout_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${binzout_server.calculate()}!');
+}
---
+++ test/binzout_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:binzout_server/binzout_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
