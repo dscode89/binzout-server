08ca1f174c19250571b08a9c2287d2c7775fab2d
Daniel Saleh <danielsaleh@Mac.WestWallasey.local>
2025-12-11 11:28:15 +0000

    add dockerfile

--- test/unit_tests.dart
+++ test/unit_tests.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:binzout_server/classes/BinScheduleEvent.dart';
 import 'package:binzout_server/utilities/type_sssert_json_list.dart';
 import 'package:test/test.dart';
